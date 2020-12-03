9fbe16f6475ece5fca182c33d713c18977ea1c39
JazzyQuark <hemant.jha@gmail.com>
2020-12-02 21:11:14 -0600

    init

---
+++ startup/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from textblob import TextBlob
+
+# create Flask app
+app = Flask(__name__)
+
+# a simple home page
+@app.route('/')
+def hello():
+    return 'Python loves VS Toolbox!'
+
+# create sentiment api
+@app.route('/<message>')
+def index(message):
+    sentiment = "positive"
+    if(TextBlob(message).sentiment.polarity < 0.0):
+        sentiment = "negative"
+
+    return app.make_response(sentiment)
